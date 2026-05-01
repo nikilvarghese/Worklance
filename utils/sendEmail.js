3c51a2a52ea388d6a690047f957de0fb2f90070e
Nikil <nikil123@gmail.com>
2026-05-01 14:23:45 +0530

    update smtp config

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -2,12 +2,14 @@ import nodemailer from "nodemailer";
 
 export const sendOtpEmail = async ({ email, otp, subject }) => {
   const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
 
   await transporter.sendMail({
     from: `"Worklance" <${process.env.EMAIL_USER}>`,
